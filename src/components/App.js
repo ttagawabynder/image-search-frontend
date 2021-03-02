6d18c1475456f24e55c5ef7e55eb6ddff808d8dd
ttagawabynder <tyler.tagawa@bynder.com>
2021-03-01 18:44:02 -0800

    formatting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React, {useState} from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import SearchResults from './SearchResults/SearchResults';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 
 const App = () => {
     const [file, setFile] = useState();
     const [imageList, setImageList] = useState([]);
     return (
-        <div className="ui" style={{margin: '10px 30px 30px 10px'}}>
-            <SearchBar file={file} setFile={setFile} setImageList={setImageList}/>
-            {imageList.length > 0 && <SearchResults images={imageList} file={file}/>}
+        <div className="ui">
+            <BrowserRouter>
+                <Route path="/" exact>
+                    <SearchBar file={file} setFile={setFile} setImageList={setImageList}/>
+                </Route>
+                <Route path="/results" exact>
+                    {imageList.length > 0 && <SearchResults images={imageList} file={file}/>}
+                </Route>
+            </BrowserRouter>
         </div>
     );
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
+import { useHistory } from "react-router-dom";
+import "./SearchBar.css";
 
 const SearchBar = ( {file, setFile, setImageList} ) => {
+    let history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
     const onInputChange = ({ target }) => {
         console.log(target.files);
         const file = target.files.length ? target.files[0] : null;
@@ -16,20 +20,25 @@ const SearchBar = ( {file, setFile, setImageList} ) => {
 
     const onSearchSubmit = async (e) => {
         e.preventDefault();
+        setIsLoading(true);
         const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
         setImageList(response.data.slice(0, 30));
+        setIsLoading(false);
+        history.push('/results');
         console.log(response);
     }
 
     return (
-        <div className="ui segment search-bar">
-            <form className="ui form" onSubmit={onSearchSubmit}>
-                <div className="field">
-                    <label>Image Search</label>
-                    <input type="file" onChange={onInputChange}/>
-                </div>
-                <button className={`ui button ${file ? '' : 'disabled'}`} type="submit">Search</button>
-            </form>
+        <div className="search-bar-container" style={{backgroundImage: `url("/images/keanuSerious.png")`}}>
+            <div className="search-bar">
+                <form className="ui form" onSubmit={onSearchSubmit}>
+                    <div className="search-file-selector">
+                        <label id="file-selector-label" for="file-selector">{file ? file.filename : "Search for related images"}</label>
+                        <input id="file-selector" style={{display: "None"}}type="file" onChange={onInputChange}/>
+                    </div>
+                    <input className="search-button" type="image" src="/images/searchButton.png" onClick={onSearchSubmit}/>
+                </form>
+            </div>
         </div>
     );
 }
