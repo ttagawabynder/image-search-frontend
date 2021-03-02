dd694ca98a6189e60b809e00ea7f89d5669fcdeb
ayohanes <ayohannes@webdam.com>
2021-03-02 15:42:32 -0600

    First working version

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -15,35 +15,35 @@ const SearchResults = ({images, file}) => {
     const goodResults = [];
     const okResults = [];
 
-    for (const [path, score] of Object.entries(images)) {
-        const pathPieces = path.split('/');
-        console.log('path pieces', pathPieces);
-        const newPath = `/images/image_data/${pathPieces[pathPieces.length-2]}/${pathPieces[pathPieces.length-1]}`;
-        const fileData = {src: newPath, filename: pathPieces[pathPieces.length-1], score: score.toFixed(2)};
-        if (path == "/Users/tylertagawa/Sites/image-search/public/images/image_data/nina/nina_face.png") {
-            for (let i = 0; i<10; i++) {
-                // goodResults.push(fileData)
-                exactResults.push(fileData);
-            }
-        } else {
-            for (let i = 0; i<10; i++) {
-                okResults.push(fileData)
-            }
-        }
-    }
-
     // for (const [path, score] of Object.entries(images)) {
     //     const pathPieces = path.split('/');
+    //     console.log('path pieces', pathPieces);
     //     const newPath = `/images/image_data/${pathPieces[pathPieces.length-2]}/${pathPieces[pathPieces.length-1]}`;
     //     const fileData = {src: newPath, filename: pathPieces[pathPieces.length-1], score: score.toFixed(2)};
-    //     if (score < 0.3) {
-    //         exactResults.push(fileData);
-    //     } else if (score < 0.5) {
-    //         goodResults.push(fileData);
+    //     if (path == "/Users/tylertagawa/Sites/image-search/public/images/image_data/nina/nina_face.png") {
+    //         for (let i = 0; i<10; i++) {
+    //             // goodResults.push(fileData)
+    //             exactResults.push(fileData);
+    //         }
     //     } else {
-    //         okResults.push(fileData);
+    //         for (let i = 0; i<10; i++) {
+    //             okResults.push(fileData)
+    //         }
     //     }
     // }
+
+    for (const [path, score] of Object.entries(images)) {
+        const pathPieces = path.split('/');
+        const newPath = `/images/image_data/${pathPieces[pathPieces.length-2]}/${pathPieces[pathPieces.length-1]}`;
+        const fileData = {src: newPath, filename: pathPieces[pathPieces.length-1], score: score.toFixed(2)};
+        if (score < 0.3) {
+            exactResults.push(fileData);
+        } else if (score < 0.5) {
+            goodResults.push(fileData);
+        } else {
+            okResults.push(fileData);
+        }
+    }
 
     console.log("good results", goodResults);
     console.log("ok results", okResults);
